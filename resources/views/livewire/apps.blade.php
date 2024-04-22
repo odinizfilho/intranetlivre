e6bd66116d235f727b0070e571d555c43a487457
Gabriel Diniz <bonjornogabriel@gmail.com>
2024-04-22 08:49:16 -0300

    Update apps.blade.php

--- resources/views/livewire/apps.blade.php
+++ resources/views/livewire/apps.blade.php
@@ -7,7 +7,7 @@
             @foreach ($apps as $app)
                 @if ($app->active)
                     <!-- item-inicio -->
-                    <a href="{{ $app->link }}" target="_blank" title="{{ $app->name }}">
+                    <a href="{{ $app->app_link }}" target="_blank" title="{{ $app->name }}">
                         <div
                             class="flex flex-col items-center  rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
                             <img src="{{ $app->image_url }}" alt="{{ $app->name }}" width="40" height="40"
